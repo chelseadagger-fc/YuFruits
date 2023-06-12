41c7e1a21c434079d17bad352887072729823171
Daniel <daniel.aldridge@alumni.unc.edu>
2023-06-12 14:58:15 +0300

    refactored using mongoose

--- app.js
+++ app.js
@@ -1,48 +1,75 @@
-const { MongoClient } = require('mongodb');
+const mongoose = require("mongoose");
  
-// Connection URL
-const url = 'mongodb://127.0.0.1:27017';
-const client = new MongoClient(url);
- 
-// Database Name
-const dbName = 'fruitsDB';
+//connect to MongoDB by specifying port to access MongoDB server
+main().catch((err) => console.log(err));
  
 async function main() {
-    // Use connect method to connect to the server
-    await client.connect();
-    console.log('Connected successfully to server');
-    const db = client.db("fruitsDB");
-    const collection = db.collection('fruits');
- 
-    const insertResult = await collection.insertMany(
-        [
-            {
-                name: "Apple",
-                score: 8,
-                review: "So Healthy"
-            },
-            {
-                name: "Orange",
-                score: 7,
-                review: "Good for Health"
-            },
-            {
-                name: "Banana",
-                score: 10,
-                review: "Awesome Taste"
-            }
-        ]);
-    console.log('Inserted documents =>', insertResult);
- 
-    const findResult = await collection.find({}).toArray();
-    console.log('Found documents =>', findResult);
- 
-    // the following code examples can be pasted here...
- 
-    return 'done.';
+  mongoose.set("strictQuery", false);
+  await mongoose.connect("mongodb://127.0.0.1:27017/fruitsDB");
 }
  
-main()
-    .then(console.log)
-    .catch(console.error)
-    .finally(() => client.close());
+//create a SCHEMA that sets out the fields each document will have and their datatypes
+ 
+const fruitSchema = new mongoose.Schema({
+  name: String,
+  rating: Number,
+  review: String,
+});
+ 
+//create a MODEL
+const Fruit = new mongoose.model("Fruit", fruitSchema);
+ 
+//create a DOCUMENT
+const fruit = new Fruit({
+  name: "Apple",
+  rating: 9,
+  review: "Great fruit. A classic.",
+});
+ 
+//save the document
+fruit.save();
+ 
+//**CHALLENGE: Set up a people database with one document and two fields**//
+//create a SCHEMA
+const personSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+});
+ 
+//create a MODEL
+const Person = mongoose.model("Person", personSchema);
+ 
+//create a DOCUMENT
+const person = new Person({
+  name: "John",
+  age: 37,
+});
+ 
+//Save it
+person.save();
+ 
+const kiwi = new Fruit({
+  name: "Kiwi",
+  score: 8,
+  review: "The Best Fruit",
+});
+ 
+const orange = new Fruit({
+  name: "Orange",
+  score: 4,
+  review: "Too sour for me",
+});
+ 
+const banana = new Fruit({
+  name: "Banana",
+  score: 3,
+  review: "Weird texture",
+});
+ 
+Fruit.insertMany([kiwi, orange, banana])
+  .then(function () {
+    console.log("Data inserted"); // Success
+  })
+  .catch(function (error) {
+    console.log(error); // Failure
+  });
